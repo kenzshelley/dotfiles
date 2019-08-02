0d7eb8a19709d851b835dc371a53fbc44d86315a
Mackenzie Shelley <mshelley@squareup.com>
2019-08-02 15:42:52 -0700

    move stuff into config folder

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-ln -s ~/Development/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/Development/dotfiles/zshrc ~/.zshrc
-ln -s ~/Development/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/Development/dotfiles/config/vim/.vimrc ~/.vimrc
+ln -s ~/Development/dotfiles/config/zshrc ~/.zshrc
+ln -s ~/Development/dotfiles/config/.tmux.conf ~/.tmux.conf

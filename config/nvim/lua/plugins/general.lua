371cae04cdd59c9d18fc273e61356f2a7e56007b
Mackenzie Shelley <kenzshelley@gmail.com>
2026-04-25 16:33:58 -0700

    enable signature hints

--- config/nvim/lua/plugins/general.lua
+++ config/nvim/lua/plugins/general.lua
@@ -21,6 +21,7 @@ return {
         documentation = { auto_show = true, auto_show_delay_ms = 200 },
         ghost_text = { enabled = true },
       },
+      signature = { enabled = true },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
       },
